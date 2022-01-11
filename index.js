6f8b2f56b18601ed69cf93082d161f9f5743203a
Michael <MichaelCheung101@gmail.com>
2022-01-11 09:49:56 -0500

    commit tree d1a6ac3c7c23128c61ca53201a2babd6619ca86c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const saturdayFun =(activity='roller-skate') =>{
+  return `This Saturday, I want to ${activity}!`  
+}
+
+const mondayWork = (activity = 'go to the office') => {
+ return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective =(symbol = '*') => {
+ return `You are ${symbol}!`   
+}
